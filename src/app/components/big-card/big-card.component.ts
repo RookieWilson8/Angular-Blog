7a4d5bd569e56f4629bbaf2400463f8f67bc0ef9
RookieWilson8 <111396921+RookieWilson8@users.noreply.github.com>
2023-09-16 13:01:33 -0300

    dynamics components created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent  {
 
+  @Input()
+  photocover: string =""
+  @Input()
+  cardtitle: string =""
+  @Input()
+  cardDescription: string =""
 }
